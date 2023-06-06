b7f17ced2aac828a9fdbd8f52e9382e134734fb2
Collin Meyer <collinmmeyer@gmail.com>
2023-06-06 08:25:00 -0400

    got all channel ids for all teams

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from mattermostdriver import Driver
 import yaml
+from collections import defaultdict
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 auth_file = open('token.yml', 'r')
 auth = yaml.load(auth_file, yaml.CLoader)
@@ -12,5 +16,13 @@ d = Driver({
 
 d.login()
 
-team_ids = [t["id"] for t in d.teams.get_teams()]
-    
+# teams = [(t["name"], t["id"]) for t in d.teams.get_teams()]
+# team_ids = {t[0]: t[1] for t in teams}
+
+# channels = defaultdict(list)
+
+# for team_name, team_id in teams:
+#     for channel in d.channels.search_channels(team_id):
+#         channels["team_name"].append(channel)
+
+print(len(d.client.get(f"/channels/isg6cwttn3rbink85ys91ajuee/posts")["posts"].keys()))
